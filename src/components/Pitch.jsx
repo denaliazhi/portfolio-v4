dc2a4aaf2cde75bec1319388d7ebd6b21ec188dc
Denalia Zhi <74125868+denaliazhi@users.noreply.github.com>
2025-11-03 23:08:03 -0500

    feat: pitch text

---
+++ src/components/Pitch.jsx
@@ -0,0 +1,28 @@
+export default function Pitch() {
+  return (
+    <>
+      <section id="pitch">
+        <div>
+          <h2>What's that saying?</h2>
+          <p>"Nothing exists in a vacuum."</p>
+          <p>That idea drives how I work.</p>
+        </div>
+        <div>
+          <p>
+            Besides wireframing and writing code,
+          </p>
+          <p>
+            I like to build bridges between people
+            & products.
+          </p>
+        </div>
+        <div>
+          <p>
+            I'm a communicator as much as a
+            creator.
+          </p>
+        </div>
+      </section>
+    </>
+  );
+}
