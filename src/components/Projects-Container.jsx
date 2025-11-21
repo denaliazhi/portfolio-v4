2220d8d58923022840aeb67ee1093c37b9ba35e4
Denalia Zhi <74125868+denaliazhi@users.noreply.github.com>
2025-11-21 18:48:20 -0500

    style: work in progress

--- src/components/Projects-Container.jsx
+++ src/components/Projects-Container.jsx
@@ -1,44 +1,68 @@
+import { useRef } from "react";
+
 export default function ProjectsContainer({
   content,
 }) {
+  const projectsRef = useRef();
+  const handleClick = (e) => {
+    e.preventDefault();
+    projectsRef.current.scrollIntoView({
+      behavior: "smooth",
+    });
+  };
+
   return (
     <section className="projects">
-      <h2>Read about my latest work</h2>
-      <div>
-        {Object.values(content).map((project) => (
-          <a
-            key={project.title}
-            href={`/${project.title.replaceAll(
-              " ",
-              "-"
-            )}`}
-          >
-            <figure
-              className={"image-caption modern"}
+      <a
+        href="#latest-projects"
+        onClick={handleClick}
+      >
+        <h2>Read about my latest work</h2>
+      </a>
+      <div id="latest-projects" ref={projectsRef}>
+        {Object.values(content).map((project) => {
+          return (
+            <a
+              className={
+                project.title === "Orbitl"
+                  ? "wip"
+                  : null
+              }
+              key={project.title}
+              href={
+                project.title === "Orbitl"
+                  ? null
+                  : `/${project.title.replaceAll(
+                      " ",
+                      "-"
+                    )}`
+              }
             >
-              <img
-                src={`/projects/${project.images_featured[0].src}`}
-                alt={
-                  project.images_featured[0].alt
-                }
-              />
-              <figcaption>
-                <h3>{project.title}</h3>
-                <p>{project.desc_short}</p>
-                <ul className="tags">
-                  <li>{project.type}</li>
-                  {project.tools.map((tool) => (
-                    <li
-                      key={`${project.title}-${tool}`}
-                    >
-                      {tool}
-                    </li>
-                  ))}
-                </ul>
-              </figcaption>
-            </figure>
-          </a>
-        ))}
+              <figure className="image-caption modern">
+                <img
+                  src={`/projects/${project.images_featured[0].src}`}
+                  alt={
+                    project.images_featured[0].alt
+                  }
+                />
+                <figcaption>
+                  <h3>{project.title}</h3>
+                  <p>{project.desc_short}</p>
+                  <ul className="tags">
+                    <li>{project.type}</li>
+                    {project.tools.map((tool) => (
+                      <li
+                        key={`${project.title}-${tool}`}
+                      >
+                        {tool}
+                      </li>
+                    ))}
+                  </ul>
+                </figcaption>
+              </figure>
+            </a>
+          );
+        })}
       </div>
     </section>
   );
